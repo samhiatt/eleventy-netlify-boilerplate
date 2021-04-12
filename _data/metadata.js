429ab71d7497cee1831caf1753fd2347393587d3
Sam Hiatt <samhiatt@gmail.com>
2021-04-12 02:45:29 -0400

    Copyright in Footer

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,7 @@
-{
+module.exports = {
   "title": "Eleventy Netlify Boilerplate",
   "description": "A template for building a simple blog website with the Eleventy static site generator, Netlify CMS, and with deployment to Netlify.",
-  "url": "https://eleventy-netlify-boilerplate.netlify.app"
-}
+  "url": "https://eleventy-netlify-boilerplate.netlify.app",
+  "companyName": "Company",
+  "lastModified": new Date(),
+};
